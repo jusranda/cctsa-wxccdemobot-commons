8ff5affd4f6b9ce8e8168495f0bd20733e091e55
Justin Randall <jusranda@cisco.com>
2022-09-01 00:46:26 -0400

    Ensure person.phone comes up E164

--- src/common.js
+++ src/common.js
@@ -22,7 +22,7 @@ function getJdsPerson(dialogContext) {
     person.identityAlias = getIdentityAlias(dialogContext);
     person.firstName = dialogContext.params.customerFirstName;
     person.lastName = dialogContext.params.customerLastName;
-    person.phone = dialogContext.params.callingNumber;
+    person.phone = '+1'+dialogContext.params.callingNumber;
     person.email = dialogContext.params.mail;
 
     return person;
